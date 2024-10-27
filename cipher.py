5b734bfe96dd8487cb34386f80ac0a27eb4351f9
lamblamb80 <lambert.danielmarks@gmail.com>
2024-10-27 09:22:21 -0400

    Lambert Daniel-Marks Caesar Cipher code upload

--- cipher.py
+++ cipher.py
@@ -1,1 +1,19 @@
-# add your code here
+phrase = input("Please enter phrase to convert: ")
+phrase = phrase.lower()
+length = int(len(phrase))
+translated = ""
+
+for i in range(length):
+    ctr = phrase[i]
+    num = ord(ctr)                       
+    if (num >= 97) and (num <= 122):     
+        num = num + 5
+        if (num > 122):
+            diff = num - 122
+            num = 97 + diff - 1
+    ctr = chr(num)
+    translated += ctr
+    
+print("With a right shift of 5, the encoded phrase is: ", translated)
+
+
