f09ffffd930a01b2e28999c74e50bfc991ee7e85
lamblamb80 <lambert.danielmarks@gmail.com>
2024-10-28 20:38:00 -0400

    Updates to Caesar Cipher code

--- cipher.py
+++ cipher.py
@@ -1,18 +1,16 @@
 phrase = input("Please enter phrase to convert: ")
 phrase = phrase.lower()
-length = int(len(phrase))
 translated = ""
 
-for i in range(length):
-    ctr = phrase[i]
-    num = ord(ctr)                       
-    if (num >= 97) and (num <= 122):     
+for char in phrase:
+    num = ord(char)
+    if (num >= 97) and (num <= 122):
         num = num + 5
         if (num > 122):
             diff = num - 122
             num = 97 + diff - 1
-    ctr = chr(num)
-    translated += ctr
+    new_char = chr(num)
+    translated += new_char
     
 print("With a right shift of 5, the encoded phrase is: ", translated)
 
